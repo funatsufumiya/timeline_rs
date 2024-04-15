e5b335527e34454a6b47919e6e0afa3694648cac
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-04-15 14:43:42 +0900

    updated xml api

--- src/xml_to_json.rs
+++ src/xml_to_json.rs
@@ -1,13 +1,19 @@
-use quickxml_to_serde::{xml_str_to_json, xml_string_to_json, Config};
+use quickxml_to_serde::Config;
 use lazy_static::lazy_static;
 
 lazy_static! {
     static ref XML_CONFIG_DEFAULT: Config = Config::new_with_defaults();
 }
 
-pub fn xml_to_json(xml: &str) -> Result<serde_json::Value, minidom::error::Error> {
+pub fn xml_str_to_json(xml: &str) -> Result<serde_json::Value, minidom::error::Error> {
     let config = &*XML_CONFIG_DEFAULT;
-    let json = xml_str_to_json(xml, config);
+    let json = quickxml_to_serde::xml_str_to_json(xml, config);
+    json
+}
+
+pub fn xml_string_to_json(xml: String) -> Result<serde_json::Value, minidom::error::Error> {
+    let config = &*XML_CONFIG_DEFAULT;
+    let json = quickxml_to_serde::xml_string_to_json(xml, config);
     json
 }
 
@@ -18,7 +24,7 @@ mod tests {
     #[test]
     fn test_xml_to_json() {
         let xml = r#"<root><a>1</a><b>2</b></root>"#;
-        let json = xml_to_json(xml).unwrap();
+        let json = xml_str_to_json(xml).unwrap();
         let expected = serde_json::json!({
             "root": {
                 "a": 1,
@@ -31,7 +37,7 @@ mod tests {
     #[test]
     fn test_xml_attr_to_json() {
         let xml = r#"<root a="1" b="2"></root>"#;
-        let json = xml_to_json(xml).unwrap();
+        let json = xml_str_to_json(xml).unwrap();
         let expected = serde_json::json!({
             "root": {
                 "@a": 1,
@@ -44,7 +50,7 @@ mod tests {
     #[test]
     fn test_xml_attr_and_child_to_json() {
         let xml = r#"<root a="1"><b>2</b></root>"#;
-        let json = xml_to_json(xml).unwrap();
+        let json = xml_str_to_json(xml).unwrap();
         let expected = serde_json::json!({
             "root": {
                 "@a": 1,
